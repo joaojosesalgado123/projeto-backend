58736246ad7962f2be76ef7d0b81a5e551e22690
joaojosesalgado123 <joaojosesalgado@ipvc.pt>
2025-03-20 16:31:56 +0000

    Ultima mudança antes da apresnetação do final da 1ªsprint

--- ES2_TPratico/ES2Backend/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace ES2Backend.Models
-{
-    public class LoginRequest
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
